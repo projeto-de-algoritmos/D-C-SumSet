fc13ad63b60c02f34611db0e2c851e5d29a4688b
Rhuan Queiroz <rhuancarlos.queiroz@gmail.com>
2021-09-23 20:30:39 -0300

    Função para somar conjuntos utilizando fast exp.
    Co-authored-by: ThiagoGMF <thiago.guilherme20@hotmail.com>

--- sum_set.py
+++ sum_set.py
@@ -26,3 +26,28 @@ def sum_set(A: set, B: set) -> set:
         res.add(i)
 
     return res
+
+def sum_set_nary(A: set, n : int = 1) -> set:
+    if n == 1:
+        return A
+
+    res = []
+    if n % 2 == 0:
+        res = sum_set_nary(A, n // 2)
+    else:
+        res = sum_set_nary(A, n - 1)
+
+    if n % 2 == 0:
+        res = sum_set(res, res)
+    else:
+        res = sum_set(res, A)
+
+    return res
+
+
+        
+if __name__ == "__main__":
+    A = {3,4,5}
+    print(sum_set_nary(A, 1))
+    print(sum_set_nary(A, 2))
+    print(sum_set_nary(A, 3))

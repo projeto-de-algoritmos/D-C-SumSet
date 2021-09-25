ca3d0587bfc30c2d61c1620f32b7d242005269c5
Rhuan Queiroz <rhuancarlos.queiroz@gmail.com>
2021-09-25 14:05:15 -0300

    Criando endpoint para soma de conjuntos
    Co-authored-by: ThiagoGMF <thiago.guilherme20@hotmail.com>

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_restful import Resource, Api
+from sum_set import sum_set_nary
 
 app = Flask(__name__)
+api = Api(app)
 
+class SumSet(Resource):
+    def post(self):
+        data = request.get_json()
+        req_set = data['set']
+        n = data['n']
+        sum_set = sum_set_nary(set(req_set), n)
+        return jsonify({'sum_set': list(sum_set)})
+
+api.add_resource(SumSet, '/sumset')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)

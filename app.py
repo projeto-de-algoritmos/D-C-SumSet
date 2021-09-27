79a079d176da572580c627453c89531b76bff1bd
Rhuan Queiroz <rhuancarlos.queiroz@gmail.com>
2021-09-27 20:47:38 -0300

    Adicionando endpoint para soma, e separando do anterior
    Co-authored-by: ThiagoGMF <thiago.guilherme20@hotmail.com>

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
-from sum_set import sum_set_nary
+from sum_set import sum_set_nary, sum_set 
 
 app = Flask(__name__)
 api = Api(app)
@@ -8,12 +8,21 @@ api = Api(app)
 class SumSet(Resource):
     def post(self):
         data = request.get_json()
+        set_A = data['A']
+        set_B = data['B']
+        sum = sum_set(set(set_A), set(set_B))
+        return jsonify({'sum_set': list(sum)})
+
+class Multiply(Resource):
+    def post(self):
+        data = request.get_json()
         req_set = data['set']
         n = data['n']
         sum_set = sum_set_nary(set(req_set), n)
-        return jsonify({'sum_set': list(sum_set)})
+        return jsonify({'multiply': list(sum_set)})
 
 api.add_resource(SumSet, '/sumset')
+api.add_resource(Multiply, '/multiply')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
